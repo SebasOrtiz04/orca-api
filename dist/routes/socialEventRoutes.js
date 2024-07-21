94e5f0f73e999f2d423a019b3986e8a1600c2000
Juan Sebastian Ortiz Castro <93163636+SebasOrtiz04@users.noreply.github.com>
2024-07-21 17:18:30 -0600

    fixin back

--- dist/routes/socialEventRoutes.js
+++ dist/routes/socialEventRoutes.js
@@ -4,5 +4,6 @@ const express_1 = require("express");
 const SocialEventController_1 = require("../controllers/products/SocialEventController");
 const router = (0, express_1.Router)();
 router.get('/', SocialEventController_1.SocialEventController.getAllSocialevents);
+router.put('/', SocialEventController_1.SocialEventController.getAllSocialevents);
 exports.default = router;
 //# sourceMappingURL=socialEventRoutes.js.map
