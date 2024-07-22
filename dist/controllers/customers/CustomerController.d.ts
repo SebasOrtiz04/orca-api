a2d7b1ed047845e399d406c4dbccbf9258b09272
Juan Sebastian Ortiz Castro <93163636+SebasOrtiz04@users.noreply.github.com>
2024-07-21 19:22:10 -0600

    Delete dist directory

--- dist/controllers/customers/CustomerController.d.ts
+++
@@ -1,8 +0,0 @@
-import type { Request, Response } from 'express';
-export declare class CustomerController {
-    static createCustomer: (req: Request, res: Response) => Promise<void>;
-    static getAllCustomers: (req: Request, res: Response) => Promise<void>;
-    static getCustomerById: (req: Request, res: Response) => Promise<Response<any, Record<string, any>>>;
-    static updateCustomer: (req: Request, res: Response) => Promise<Response<any, Record<string, any>>>;
-    static deleteCustomer: (req: Request, res: Response) => Promise<Response<any, Record<string, any>>>;
-}
--- dist/controllers/products/SocialEventController.d.ts
+++
@@ -1,5 +0,0 @@
-import type { Request, Response } from 'express';
-export declare class SocialEventController {
-    static createEvent: (req: Request, res: Response) => Promise<Response<any, Record<string, any>>>;
-    static getAllSocialevents: (req: Request, res: Response) => Promise<Response<any, Record<string, any>>>;
-}
