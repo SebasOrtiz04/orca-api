c52b166f2f1b1b657844b0d65b07a64891ae88ed
Juan Sebastian Ortiz Castro <93163636+SebasOrtiz04@users.noreply.github.com>
2024-11-20 02:32:18 -0600

    image CRUD

--- src/routes/imageRoutes.ts
+++ src/routes/imageRoutes.ts
@@ -4,16 +4,27 @@ import upload from "../config/multerConfig";
 import { fileFilter } from "../middleware/images/imagesMulter";
 import { validateUploadImage } from "../middleware/images/image";
 import { handleInputErrors } from "../middleware/validation";
+import { autenticate } from "../middleware/auth";
 
 
 const router = Router();
 
 router.post('/',
+    autenticate,
     upload.single('image'),
     fileFilter,
     validateUploadImage,
     handleInputErrors,
     ImageController.uploadImage
+)
+
+router.get('/',
+    ImageController.getAllImages
+)
+
+router.delete('/:id',
+    autenticate,
+    ImageController.deleteImage
 )
 
 export default router;
